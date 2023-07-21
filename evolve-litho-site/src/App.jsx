4625dda6aaef088420b37f45f3deb04ca95dc713
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-21 17:45:43 +0100

    Added success page

--- evolve-litho-site/src/App.jsx
+++ evolve-litho-site/src/App.jsx
@@ -5,13 +5,15 @@ import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Privacypolicy from "./components/Privacypolicy";
+import Success from "./components/Success";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/privacypolicy' element={<Privacypolicy />} />
+        <Route path="/privacypolicy" element={<Privacypolicy />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
       <Navbar />
       <Hero />
--- evolve-litho-site/src/components/Contact.jsx
+++ evolve-litho-site/src/components/Contact.jsx
@@ -1,4 +1,37 @@
+import { useFormik } from "formik";
+import * as Yup from 'yup'
+import { useNavigate } from 'react-router-dom'
+
 const Contact = () => {
+
+  const nav = useNavigate()
+
+  // Formik Logic
+const formik = useFormik({
+  initialValues: {
+    name: '',
+    email: '',
+    subject: '', 
+    message: '',
+    pp: '',
+  },
+
+  // Validate form
+  validationSchema: Yup.object({
+    name: Yup.string().max(20, 'Name must not be longer than 20 characters'),
+    email: Yup.string().email('Invalid email address'),
+    subject: Yup.string(),
+    message: Yup.string(),
+    pp: Yup.array().required('You must agree to our Privacy Policy')
+  }),
+
+  // Submit form
+  onSubmit: (values) => {
+    console.log(values);
+    nav('/success')
+  }
+})
+  
   return (
     <div
       name="contact"
@@ -7,9 +40,8 @@ const Contact = () => {
       <form
         name="contact-form"
         method="POST"
-        action="/contact"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
+        action="/"
+        onSubmit={formik.handleSubmit}
         className="flex flex-col max-w-[600px] w-full text-center"
       >
         <div className="pb-8 ">
@@ -17,40 +49,68 @@ const Contact = () => {
             Contact
           </p>
         </div>
-        <input type="hidden" name="form-name" value="contact-form"/>
+
         <input
           className="my-2 p-2 text-black rounded-md"
           type="text"
           placeholder="Name"
           name="name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
           required
         />
+
         <input
           className="my-2 p-2 text-black rounded-md"
           type="email"
           placeholder="Email"
           name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
           required
         />
+
         <input
           className="my-2 p-2 text-black rounded-md"
           type="text"
           placeholder="Subject"
           name="subject"
+          value={formik.values.subject}
+          onChange={formik.handleChange}
           required
         />
+
         <textarea
           className="my-2 p-2 resize-none text-black rounded-md"
           placeholder="Enter your message here..."
           name="message"
+          value={formik.values.message}
+          onChange={formik.handleChange}
           rows="10"
           required
         ></textarea>
+
         <div className="flex p-4">
-          <input className='w-4 text-blue-600 bg-gray-100 border-gray-300 rounded' type="checkbox" id="contact-form-checkbox" required />
-          <label className="ml-2 text-sm font-medium dark:text-gray-300 text-white" htmlFor="contact-form-checkbox">By checking this box you agree with our <a href="/privacypolicy" className="text-blue-600 dark:text-blue-500 hover:underline">Privacy Policy</a></label>
+          <input
+            className="w-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+            type="checkbox"
+            value='checked'
+            onChange={formik.handleChange}
+            id="contact-form-checkbox"
+            required
+          />
+
+          <label
+            className="ml-2 text-sm font-medium dark:text-gray-300 text-white"
+            htmlFor="contact-form-checkbox"
+          >
+            By checking this box you agree with our{" "}<a href="/privacypolicy" className="text-blue-600 dark:text-blue-500 hover:underline">Privacy Policy</a>
+          </label>
         </div>
-        <button type='submit' className="rounded-md border-2 hover:bg-gray-400 hover:border-grbg-gray-400 px-6 py-2 my-2 mx-auto flex items-center text-who text-white">
+        <button
+          type="submit"
+          className="rounded-md border-2 hover:bg-gray-400 hover:border-grbg-gray-400 px-6 py-2 my-2 mx-auto flex items-center text-who text-white"
+        >
           Submit
         </button>
       </form>
